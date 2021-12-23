5be1ee97c25fb794e484f2af5ac065b8efeb524a
Skep <madskep@gmail.com>
2021-12-23 23:33:03 +0200

    Add fixed header on scroll

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -18,4 +18,18 @@ window.onload = function () {
             document.querySelector('.search-form').classList.remove('_active');
         }
     }
+
+    //Fixed header
+    const headerElement = document.querySelector('.header');
+
+    const callback = function (entries, observer) {
+        if (entries[0].isIntersecting) {
+            headerElement.classList.remove('_scroll');
+        } else {
+            headerElement.classList.add('_scroll');
+        }
+    };
+
+    const headerObserver = new IntersectionObserver(callback);
+    headerObserver.observe(headerElement);
 }
